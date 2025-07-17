219ddfc2da80be3aa07919a73b462b9f1f762752
TranThanhAn20225592 <An.TT225592@sis.hust.edu.vn>
2025-07-17 17:45:52 +0700

    Sua lai giao dien DNA

--- lib/main.dart
+++ lib/main.dart
@@ -32,100 +32,128 @@ class _LoginScreenState extends State<LoginScreen> {
         FocusScope.of(context).unfocus();
       },
       child: Scaffold(
-        resizeToAvoidBottomInset: false,  // Cố định giao diện
-        body: SafeArea(
-          child: Container(
-              width: double.infinity,
-              height: MediaQuery.of(context).size.height,
-              decoration: BoxDecoration(
-              image: DecorationImage(image: AssetImage("assets/DNA_BLUE_2103.png"),
-
-          //Tao nen mau boc ngoai anh
-              fit: BoxFit.cover,
-              colorFilter: ColorFilter.mode(Color(0xFF4B1C6F).withOpacity(0.8),BlendMode.darken,),
-            ),
-           ),
-          
-
-          child: Column(
+        resizeToAvoidBottomInset: false, 
+        body: SafeArea( // Cố định giao diện
+          child: Stack(
             children:[
-              // Tao logo DNAfit
-              SizedBox(height:120),
-              Text("DNAfit", style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 232, 233, 159), letterSpacing: 1.2)),
-              
-              // Tao o TextField nhap Username hoac Email
-              SizedBox(height: 100),
-              TextField(
-                style: TextStyle(color: Colors.white),
-                decoration: InputDecoration(
-                  hintText: "USERNAME OR EMAIL",
-                  hintStyle: TextStyle(color: Colors.white),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white)
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white)
+               // Hinh nen va logo co dinh
+               Container(
+                 width: double.infinity,
+                 height: double.infinity,
+                 decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/DNA_BLUE_2103.png"),
+                    fit: BoxFit.cover,
+                    colorFilter: ColorFilter.mode(Color(0xFF4B1C6F).withOpacity(0.8), BlendMode.darken,)
                   )
-                ),
-              ),
+                 ),
+              // Tao logo
+                 alignment: Alignment(0, -0.75),
+                 child: Text("DNAfit", style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 232, 233, 159)))
+               ),
 
-               // Tao o TextField nhap Password
-              SizedBox(height: 10),
-              TextField(
-                style: TextStyle(color: Colors.white),
-                decoration: InputDecoration(
-                  hintText: "PASSWORD",
-                  hintStyle: TextStyle(color: Colors.white),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white)
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white)
-                  )
-                ),
-              ),
-              
-              //Tao nut Sign In
-              SizedBox(height: 120),
-              SizedBox(
-                height: 70,
-                width: 400,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.deepOrange,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadiusGeometry.zero
-                    )
-                  ),
-                  onPressed: (){}, 
-                  child: (
-                    const Text("SIGN IN", style: TextStyle(color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold))
-                  )
-                ),
-              ),
-              
-              // Tao nut forgot password
-              SizedBox(height: 5),
-              SizedBox(
-                height: 40,
-                width: 400,
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                     foregroundColor: Colors.white
-                  ),
-                  onPressed: (){}, 
-                  child: (
-                    const Text("Forgot Password"))
-                )
-              ),
-            ]
-          )
+            // Text Field cuon len khi ban phim bi che
+               Container(
+                 alignment: Alignment.center,
+                 child: Padding(
+                   padding: const EdgeInsets.symmetric(horizontal: 30),
+                   child: Column(
+                     mainAxisSize: MainAxisSize.min,
+                     children: [
+                       SizedBox(height: 235),
+                       // Dung SingleChildScrollView de cuon 
+                       SingleChildScrollView(
+                         padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom,),
+
+                      
+                         child: Column(
+                           children: [
+                             //Textfield dien User
+                             TextField(
+                               style: TextStyle(color: Colors.white),
+                               decoration: InputDecoration(
+                                 hintText: "USERNAME OR EMAIL",
+                                 hintStyle: TextStyle(color: Colors.white),
+                                 enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+                                 focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+                               ),
+                             ),
+
+                             SizedBox(height: 10),
+                             //TextField dien Password
+                             TextField(
+                               style: TextStyle(color: Colors.white),
+                               decoration: InputDecoration(
+                                 hintText: "PASSWORD",
+                                 hintStyle: TextStyle(color: Colors.white),
+                                 enabledBorder: OutlineInputBorder(
+                                   borderSide: BorderSide(color: Colors.white)
+                                 ),
+                                 focusedBorder: OutlineInputBorder(
+                                   borderSide: BorderSide(color: Colors.white)
+                                 ),
+                               ),
+                            ),
+
+                             SizedBox(height: 10),
+                           ]
+                         )
+                       )
+                     ],
+                   )
+                 )
+               ),
+            
+               // Tao nut SignIn va ForgotPassword
+               Container(
+                 alignment: Alignment(0, 0.8),
+                 child: Column(
+                   mainAxisSize: MainAxisSize.min,
+                   children: [
+                     // Tao nut Sign In
+                     SizedBox(
+                       height: 35,
+                       width: 400,
+                       child: ElevatedButton(
+                         style: ElevatedButton.styleFrom(
+                           backgroundColor: Colors.deepOrange,
+                           shape: RoundedRectangleBorder(
+                             borderRadius: BorderRadiusGeometry.zero
+                           )
+                         ),
+                         onPressed: (){}, 
+                         child: (
+                           const Text("SIGN IN", style: TextStyle(color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold))
+                         )
+                       ),
+                     ),
+
+                     SizedBox(height: 5),
+
+                     // Tao nut ForgotPassword
+                     SizedBox(
+                       height: 30,
+                       width: 400,
+                       child: TextButton(
+                         onPressed: (){}, 
+                         child: Text("Forgot Password?", style: TextStyle(color: Colors.white, fontSize: 10),)
+                       ),
+                     ),
+                   ],
+                 ),
+               ),
+             ],
+           )
          )
-        
-
-       )
       )
     );  
   }
 }
+
+
+
+
+
+
+
 
