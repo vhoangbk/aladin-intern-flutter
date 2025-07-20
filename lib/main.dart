6f59283613d16e7f350450ca56d0c4b6a96783f6
TranThanhAn20225592 <An.TT225592@sis.hust.edu.vn>
2025-07-20 22:43:02 +0700

    Thay đổi giao diện CaStore

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
+int currentIndex = 0;
 final PageController _controller = PageController();
 final List<Map<String, dynamic>> pages = [
   {
-  "image" : "assets/bong_da.jpeg",
-  "title" : "Welcome to CaStore!",
-  "subtitle" : "With long experience in the audio industry,",
-  "subtitle2" : "we create the best quality products"
+    "image": "assets/bong_da.jpeg",
+    "subtitle": "With long experience in the audio industry,",
+    "subtitle2": "we create the best quality products"
   },
-
   {
-  "image" : "assets/bong_ro.jpeg",
-  "title" : "Welcome to CaStore!",
-  "subtitle" : "Our extensive experience in the audio field",
-  "subtitle2" : "allows us to deliver top-quality products."
+    "image": "assets/bong_ro.jpeg",
+    "subtitle": "Our extensive experience in the audio field",
+    "subtitle2": "allows us to deliver top-quality products."
   },
-
   {
-  "image" : "assets/cau_long.jpeg",
-  "title" : "Welcome to CaStore!",
-  "subtitle" : "Thanks to years of expertise in audio technology,",
-  "subtitle2" : "we produce products of the highest quality."
+    "image": "assets/cau_long.jpeg",
+    "subtitle": "Thanks to years of expertise in audio technology,",
+    "subtitle2": "we produce products of the highest quality."
   }
 ];
-
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget
-{
-  const MyApp ({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context)
-  {
-    return MaterialApp
-    (
-      home: const BoardingScreen(),
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: BoardingScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class BoardingScreen extends StatefulWidget
-{
+class BoardingScreen extends StatefulWidget {
   const BoardingScreen({super.key});
 
   @override
@@ -53,112 +45,122 @@ class BoardingScreen extends StatefulWidget
 }
 
 class _BoardingScreenState extends State<BoardingScreen> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
+      body: Column(
         children: [
-          PageView.builder(     
-            controller: _controller,
-            itemCount: pages.length,
-            itemBuilder: (context, index){
-              return Column(
-                children: [
-                  Container(
-                     height: 400,
-                     width: double.infinity,
-                     child: Stack(
-                      alignment: Alignment.bottomCenter,
-                      children: [
-                         Image.asset(
-                           pages[index]["image"]!,
-                           height: 400,
-                           width: double.infinity,
-                           fit: BoxFit.cover,
-                          ),
-                         Positioned(
-                            child: SmoothPageIndicator(
-                              controller: _controller, 
-                              count: pages.length,
-                              effect: WormEffect(
-                                dotWidth: 10,
-                                dotHeight: 10,
-                                activeDotColor: Colors.yellow
-                              )
-                            ),
-                          ),
-
-                      ],)
-                  ),
-
-                  Text(
-                    pages[index]['title']!,
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
+          // PHẦN ẢNH + SMOOTH
+          Expanded(
+            child: Stack(
+              children: [
+                PageView.builder(
+                  controller: _controller,
+                  itemCount: pages.length,
+                  onPageChanged: (index) {
+                    setState(() {
+                      currentIndex = index;
+                    });
+                  },
+                  itemBuilder: (context, index) {
+                    return Image.asset(
+                      pages[index]["image"],
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                    );
+                  },
+                ),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: SmoothPageIndicator(
+                    controller: _controller,
+                    count: pages.length,
+                    effect: const WormEffect(
+                      dotWidth: 10,
+                      dotHeight: 10,
+                      activeDotColor: Colors.yellow,
                     ),
                   ),
-
-                  const SizedBox(height: 10),
-                  Text(
-                    pages[index]['subtitle']!,
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                      fontSize: 14,
-                      color: Colors.grey,
-                    ),
-                  ),
-
-                  const SizedBox(height: 10),
-                  Text(
-                    pages[index]['subtitle2']!,
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                      fontSize: 14,
-                      color: Colors.grey,
-                    ),
-                  ),
-                ],            
-              );
-            }
+                ),
+              ],
+            ),
           ),
 
-          
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Padding(             
-                padding: const EdgeInsets.only(bottom: 30),
-                child: Container(
-                  height: 90,
-                  width: 380,
-                  padding: EdgeInsets.all(16.0),               
+          // PHẦN TEXT VÀ NÚT BẤM CỐ ĐỊNH PHÍA DƯỚI
+          const SizedBox(height: 30),
+          Container(
+            //padding: const EdgeInsets.all(16.0),
+            alignment: Alignment.center,
+            child: Column(
+              children: [
+                // Phần text cố định
+                const Text(
+                  "Welcome to CaStore !",
+                  style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
+                ),
+
+                // Phần text thay đổi khi kéo 
+                const SizedBox(height: 10),
+                SizedBox(
+                  height: 20,
+                  child: AnimatedSwitcher(
+                    duration: const Duration(milliseconds: 300),
+                    child: Text(
+                      pages[currentIndex]["subtitle"] ?? '',
+                      key: ValueKey(currentIndex),
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(fontSize: 12),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                  child: AnimatedSwitcher(
+                    duration: const Duration(milliseconds: 300),
+                    child: Text(
+                      pages[currentIndex]["subtitle2"] ?? '',
+                      key: ValueKey('s2-$currentIndex'),
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(fontSize: 12),
+                    ),
+                  ),
+                ),
+
+                // NÚT BẤM
+                const SizedBox(height: 20),
+                SizedBox(
+                  width: 300,
                   child: ElevatedButton(
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.yellow,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadiusGeometry.zero
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.zero,
                       ),
-                      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
                     ),
-                    onPressed: (){}, 
-                    child: Row(  
-                       mainAxisAlignment: MainAxisAlignment.end,                 
-                       children: [
-                         Text("Get Started", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
-                         SizedBox(width: 80),
-                         Icon(Icons.arrow_forward, color: Colors.black),
-                       ],
-                    )
-                  )
-                )
-            )
-          )
-        ]
-      )
+                    onPressed: () {},
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: const [
+                        Text(
+                          "Get Started",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(width: 70),
+                        Icon(Icons.arrow_forward, color: Colors.black),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
-
